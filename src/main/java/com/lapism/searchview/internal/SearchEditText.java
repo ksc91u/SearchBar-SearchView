0bb8adb48069a9a769a786751f1c03f321de174a
lapism <lapis.martin@gmail.com>
2018-05-28 19:50:18 +0200

    fad

---
+++ src/main/java/com/lapism/searchview/internal/SearchEditText.java
@@ -0,0 +1,42 @@
+package com.lapism.searchview.internal;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.RestrictTo;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+public class SearchEditText extends AppCompatEditText {
+
+    private SearchLayout mSearchLayout;
+
+    public SearchEditText(@NonNull Context context) {
+        super(context);
+    }
+
+    public SearchEditText(@NonNull Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SearchEditText(@NonNull Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setLayout(SearchLayout layout) {
+        mSearchLayout = layout;
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, @NonNull KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+            if (mSearchLayout != null && mSearchLayout.isOpen() && hasFocus()) {
+                mSearchLayout.close();
+                return true;
+            }
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+
+}
